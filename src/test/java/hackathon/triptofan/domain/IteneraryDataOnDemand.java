7c72689ca9a3126861f963e949237b0f1997ffbf
mwarger <mwarger@gmail.com>
2014-11-15 13:57:04 -0600

    initial commit

---
+++ src/test/java/hackathon/triptofan/domain/IteneraryDataOnDemand.java
@@ -0,0 +1,6 @@
+package hackathon.triptofan.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Itenerary.class)
+public class IteneraryDataOnDemand {
+}
---
+++ src/test/java/hackathon/triptofan/domain/IteneraryIntegrationTest.java
@@ -0,0 +1,11 @@
+package hackathon.triptofan.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Itenerary.class)
+public class IteneraryIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
