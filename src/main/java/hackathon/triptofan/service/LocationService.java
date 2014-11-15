645878ea0fce514769182f73be26ac473aaae07f
gknox5411 <gknox5411@gmail.com>
2014-11-15 17:36:23 -0600

    adding in more service layer

---
+++ src/main/java/hackathon/triptofan/service/LocationService.java
@@ -0,0 +1,14 @@
+package hackathon.triptofan.service;
+
+import org.springframework.stereotype.Service;
+
+import hackathon.triptofan.domain.Location;
+
+@Service
+public class LocationService {
+	
+	public void createLocation(Location location) {
+		
+	}
+
+}
--- src/main/java/hackathon/triptofan/web/TagController.java
+++ src/main/java/hackathon/triptofan/web/TagController.java
@@ -1,5 +1,9 @@
 package hackathon.triptofan.web;
+import javax.annotation.Resource;
+
 import hackathon.triptofan.domain.Tag;
+import hackathon.triptofan.service.TagService;
+
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,4 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RooWebScaffold(path = "tags", formBackingObject = Tag.class)
 public class TagController {
+	
+	@Resource
+	TagService tagService;
 }
